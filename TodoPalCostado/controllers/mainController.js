9aa258ce25c6d28501a9aeded8f9c48759c90577
joaquinvila.g <joaquinvila@gmail.com>
2020-05-25 22:36:08 -0300

    pagina de producto, arreglos de footer y arreglos de home

--- TodoPalCostado/controllers/mainController.js
+++ TodoPalCostado/controllers/mainController.js
@@ -7,6 +7,9 @@ const controller = {
     root: function(req, res){
         res.render("home");
     },
+    ciruelasdesecadas: function(req, res){
+        res.render("ciruelasdesecadas");
+    },
     search: function(req, res){
         res.send("Resultados de busqueda");
     }
--- TodoPalCostado/routes/index.js
+++ TodoPalCostado/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const mainController = require('../controllers/mainController');
 
 /* GET home page. */
-router.get('/', mainController.root)
+router.get('/', mainController.root);
+router.get('/frutasdesecadas/ciruelasdesecadas', mainController.ciruelasdesecadas);
 
 module.exports = router;
